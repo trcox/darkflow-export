acd72b2ecf3af62ac0d7b703850e18ab23071f16
Spencer Bull <spencerbull2554@gmail.com>
2018-01-08 17:49:42 -0600

    update configs
    Signed-off-by: Spencer Bull <spencerbull2554@gmail.com>

--- sample.py
+++ sample.py
@@ -2,10 +2,10 @@ from darkflow.net.build import TFNet
 
 import cv2
 
-options = {"model": "cfg/yolo-voc.cfg", "load": "weights/yolo-voc.weights", "threshold": 0.1}
+options = {"model": "cfg/yolo-voc-hyperion.cfg", "load": "weights/yolo-voc_50000.weights", "threshold": 0.1, "labels": "image.names", "demo": "camera", "gpu": 0.8}
 tfnet = TFNet(options)
 
-imgcv = cv2.imread("./darkflow/sample_img/sample_dog.jpg")
-result = tfnet.return_predict(imgcv)
-
-print(result)
+#imgcv = cv2.imread("./darkflow/sample_img/sample_dog.jpg")
+#result = tfnet.return_predict(imgcv)
+tfnet.camera()
+exit("Demo Stopped, exiting.")
